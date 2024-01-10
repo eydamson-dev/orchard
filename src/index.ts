54ab1c839ca743d22f4360fa448a65f62cdc2f0c
Adamson Vispo <eydamson.dev@gmail.com>
2024-01-11 02:14:15 +0800

    Modify alt for SEO

--- src/index.ts
+++ src/index.ts
@@ -14,21 +14,21 @@ const cardsContainer: Element = document.getElementById('cards-container')
 const data: Article[] = [
   {
     imgSrc: 'assets/component-02/Image-01.jpg',
-    alt: 'item-1',
+    alt: 'delicious foods',
     title: 'Summer Lunch Menu By Mark Best',
     description: "AEG ambassador Mark Best's summer eats are guaranteed to help you make the most of the warmer weather and entertaining at home.",
     link: '#'
   },
   {
     imgSrc: 'assets/component-02/Image-02.jpg',
-    alt: 'item-1',
+    alt: 'chicken and sides',
     title: 'A Traditional Christmas Eve, Mark Best Style',
     description: "One of Australia's best chefs and AEG ambassador, Mark Best, shares his favourite Christmas Eve menu which is sure to impress your guests.",
     link: '#'
   },
   {
     imgSrc: 'assets/component-02/Image-03.jpg',
-    alt: 'item-1',
+    alt: 'erbs and vegetables',
     title: 'Taking Taste Further',
     description: "This exclusive cookbook gives you all the know-how you need. We've designed it to make sure you get the most out of our products - and the best out of your food.",
     link: '#'
